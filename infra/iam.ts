c2633912914f75330fa8af7e5a1939b5773b6d52
Mauricio Mercado <maumercado@gmail.com>
2024-08-23 12:37:58 -0700

    :sparkles: Enhance IAM and S3 infrastructure handling (#11)
    Refactor createIAMResources to include access key creation for IAM user
    and define policy document separately for clarity. Improve S3 bucket
    return object by adding bucket URL interpolation.

--- infra/iam.ts
+++ infra/iam.ts
@@ -4,34 +4,41 @@ import * as pulumi from "@pulumi/pulumi";
 export function createIAMResources() {
   const config = new pulumi.Config();
   const appName = config.require("appName");
-
+  // Create an IAM user
   const iamUser = new aws.iam.User(`${appName}-user`, {
     name: `${appName}-user`,
     path: "/system/",
   });
 
+  // Create an access key for the IAM user
   const accessKey = new aws.iam.AccessKey(`${appName}-access-key`, {
     user: iamUser.name,
   });
 
-  const policy = new aws.iam.UserPolicy(`${appName}-user-policy`, {
-    user: iamUser.name,
-    policy: JSON.stringify({
-      Version: "2012-10-17",
-      Statement: [{
+  // Define the policy document
+  const policyDocument = {
+    Version: "2012-10-17",
+    Statement: [
+      {
         Effect: "Allow",
         Action: [
           "s3:PutObject",
           "s3:GetObject",
           "s3:ListBucket",
-          "s3:DeleteObject"
+          "s3:DeleteObject",
         ],
         Resource: [
           pulumi.interpolate`arn:aws:s3:::${appName}-bucket/*`,
-          pulumi.interpolate`arn:aws:s3:::tooling-bucket/*`
-        ]
-      }]
-    }),
+          pulumi.interpolate`arn:aws:s3:::tooling-bucket/*`,
+        ],
+      },
+    ],
+  };
+
+  // Attach the policy to the IAM user
+  const userPolicy = new aws.iam.UserPolicy(`${appName}-user-policy`, {
+    user: iamUser.name,
+    policy: JSON.stringify(policyDocument),
   });
 
   return { iamUser, accessKey };
