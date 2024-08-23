dd4d7c0c2e8e8a305b927533ce1948d4eddc2419
Mauricio Mercado <maumercado@gmail.com>
2024-08-23 12:37:29 -0700

    :sparkles: Enhance IAM and S3 infrastructure handling
    Refactor createIAMResources to include access key creation for IAM user
    and define policy document separately for clarity. Improve S3 bucket
    return object by adding bucket URL interpolation.

--- infra/s3.ts
+++ infra/s3.ts
@@ -2,12 +2,14 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 export function createS3Bucket() {
-
-  const appBucket = new aws.s3.Bucket('Backups Bucket', {
+  const appBucket = new aws.s3.Bucket("Backups Bucket", {
     bucket: `codigo-backups`,
     acl: "private",
     forceDestroy: true,
   });
 
-  return { appBucket: pulumi.output(appBucket) };
+  return {
+    appBucket: pulumi.output(appBucket),
+    bucketUrl: pulumi.interpolate`https://${appBucket.bucket}.s3.amazonaws.com`,
+  };
 }
