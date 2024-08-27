c988c973c16c9f7a194cadd988f132430f24bea9
Mauricio Mercado <maumercado@gmail.com>
2024-08-26 20:04:11 -0700

    ✏️ infra: update Docker setup and stack deployment
    Update Docker setup script to use secure key signing method 
    and correct Docker Compose file naming for stack deployment.

--- infra/setupDockerInServer.ts
+++ infra/setupDockerInServer.ts
@@ -27,8 +27,8 @@ export const configureServer = (server: Server) => {
     create: `
       sudo apt-get update
       sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-      sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+      echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
       sudo apt-get update
       sudo apt-get install -y docker-ce
       sudo usermod -aG docker codigo
